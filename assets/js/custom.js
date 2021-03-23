223edcf0246041235ce6893bb2525c926a736a0f
Josef Ježek <2110319+JosefJezek@users.noreply.github.com>
2021-03-23 13:16:39 +0100

    Add addSimMessageHandler()

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,12 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
+
+addSimMessageHandler("web", (data) => {
+    switch(data.action) {
+        case "open":
+            const url = data.url;
+            window.open(url, "_blank");
+            break;
+    }                    
+})
